a8fbf3b6cb360aeea88cf469f9ce496e00e9cc4c
zhangsan <zanghu20080701@126.com>
2022-02-14 09:45:35 +0800

    Track 5 files into repository.
    - untracked assets/code/c_code/mmap/process_comm/build.sh
    - untracked assets/code/c_code/mmap/process_comm/Test
    - untracked assets/code/c_code/mmap/process_comm/test.c
    - untracked assets/doc/(2004_Scott Meyers)双重检查锁论文.pdf
    - untracked assets/doc/Linux x86 Program Start Up.pdf
    
    Auto commit by GitBook Editor

---
+++ assets/code/c_code/mmap/process_comm/test.c
@@ -0,0 +1,32 @@
+/**
+ * 测试通过mmap共享内存实现父子进程通信
+ * https://www.jianshu.com/p/096e1b58c678
+ */
+#include <sys/mman.h>  
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <unistd.h>  
+      
+ #define BUF_SIZE 100  
+      
+int main(int argc, char** argv)  
+{  
+    char *p_map = NULL;  
+      
+    /* 匿名映射,创建一块内存供父子进程通信 */  
+    p_map = (char *)mmap(NULL, BUF_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);  
+  
+    if(fork() == 0) {  
+        sleep(1);  
+        printf("child got a message: %s\n", p_map);  
+        sprintf(p_map, "%s", "hi, dad, this is son");  
+        munmap(p_map, BUF_SIZE); //实际上，进程终止时，会自动解除映射。  
+        exit(0);  
+    }  
+  
+    sprintf(p_map, "%s", "hi, this is father");  
+    sleep(2);  
+    printf("parent got a message: %s\n", p_map);  
+  
+    return 0;  
+}
